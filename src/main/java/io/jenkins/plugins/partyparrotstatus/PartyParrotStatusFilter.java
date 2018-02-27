7eafe2c0a974fae5213be9074d551415ed07e5aa
Nikita Belonogov <belonogovna@sgu.ru>
2018-02-27 16:01:54 +0400

    Add icons
    Add Headshot icon
    Add favicon.ico

--- src/main/java/io/jenkins/plugins/partyparrotstatus/PartyParrotStatusFilter.java
+++ src/main/java/io/jenkins/plugins/partyparrotstatus/PartyParrotStatusFilter.java
@@ -11,25 +11,27 @@ import java.util.regex.Pattern;
 public class PartyParrotStatusFilter implements Filter {
 
     final Pattern pattern = Pattern.compile("/(\\d{2}x\\d{2})/(blue|red|yellow|nobuilt|aborted|folder|grey|edit-delete|clock|disabled)(_anime|)\\.(gif|png)");
+    final Pattern anotherpattern = Pattern.compile("/(headshot)\\.(png)");
+    final Pattern yetanotherpattern = Pattern.compile("/(favicon.ico)");
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException { }
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
     @Override
-    public void destroy() { }
+    public void destroy() {
+    }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         if (req instanceof HttpServletRequest && resp instanceof HttpServletResponse) {
             final HttpServletRequest httpServletRequest = (HttpServletRequest) req;
             final HttpServletResponse httpServletResponse = (HttpServletResponse) resp;
             final String uri = httpServletRequest.getRequestURI();
-            if (uri.endsWith(".gif") || uri.endsWith(".png")) {
-                String newImageUrl = mapImage(uri);
-                if (newImageUrl != null) {
-                    RequestDispatcher dispatcher = httpServletRequest.getRequestDispatcher(newImageUrl);
-                    dispatcher.forward(httpServletRequest, httpServletResponse);
-                    return;
-                }
+            String newImageUrl = mapImage(uri);
+            if (newImageUrl != null) {
+                RequestDispatcher dispatcher = httpServletRequest.getRequestDispatcher(newImageUrl);
+                dispatcher.forward(httpServletRequest, httpServletResponse);
+                return;
             }
         }
         chain.doFilter(req, resp);
@@ -40,6 +42,14 @@ public class PartyParrotStatusFilter implements Filter {
             Matcher m = pattern.matcher(uri);
             if (m.find()) {
                 return "/plugin/partyparrotstatus/" + m.group(1) + "/" + m.group(2) + m.group(3) + "." + m.group(4);
+            }
+            Matcher anotherm = anotherpattern.matcher(uri);
+            if (anotherm.find()) {
+                return "/plugin/partyparrotstatus/origin/" + anotherm.group(1) + "." + anotherm.group(2);
+            }
+            Matcher yetanotherm = yetanotherpattern.matcher(uri);
+            if (yetanotherm.find()) {
+                return "/plugin/partyparrotstatus/origin/" + yetanotherm.group(1);
             }
         }
         return null;
